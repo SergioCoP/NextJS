8e6c928f217dee8f12bf5433a0b6b226f127f843
20203tn125 <20203tn125@utez.edu.mx>
2024-02-08 20:14:35 -0600

    [UPDATE] Avance curso next, comenzando capitulo 13

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,39 +1,30 @@
 import { Suspense } from "react";
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data"
-import { Card } from "../ui/dashboard/cards";
+import  CardWrapper from "../ui/dashboard/cards";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
+import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
 //EWsto es un react server component
 export default async function Page(){
-    const cardData = await fetchCardData()
-    
-    
+
     return  (
         <main>
           <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
             Dashboard
           </h1>
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
-            <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
-            <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
-            <Card
-              title="Total Customers"
-              value={cardData.numberOfCustomers}
-              type="customers"
-            />
+            <Suspense fallback={<CardSkeleton/>}>
+            <CardWrapper />
+            </Suspense>
           </div>
           <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
             <Suspense fallback={<RevenueChartSkeleton/>}>
-            <RevenueChart  /> 
+            <RevenueChart  />
             </Suspense>
             <Suspense fallback={<LatestInvoicesSkeleton/>}>
             <LatestInvoices  />
             </Suspense>
-            
           </div>
         </main>
       );
